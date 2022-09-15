16f259d956b927afd7f7d96dcc0615785cf6bd9a
PEJ <PEJ@DESKTOP-F55B9BB>
2022-09-15 15:06:31 +0900

    1. ch01의 초기 버전 Up

---
+++ ch01/src/main/java/ch01/Add3.java
@@ -0,0 +1,61 @@
+package ch01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Add3
+ */
+@WebServlet("/Add3")
+public class Add3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Add3() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 목표 : 1부터 누적값 전달
+		int num = Integer.parseInt(request.getParameter("num")); //request로 보내준 건 전부 String으로 넘어옴
+		String loc = request.getParameter("loc");
+		System.out.println("Add3 num->"+num);
+		int sum = 0;
+		for (int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		
+		request.setCharacterEncoding("utf-8"); //request에 한글이 들어온다면 이것도 해 주어야 함 
+		response.setContentType("text/html;charset=utf-8"); //이거 안 하면 한글 깨짐
+		//공식 --> 사용자 Browser에 보여주는 객체   response 결과값을 response에 담아서 보여줌
+		PrintWriter out = response.getWriter(); 
+		out.println("<html><body>");
+		out.printf("<h1>1부터 %d까지 합계</h1>",num);
+		out.printf("<h4>LOC--> %s</h4>",loc);
+		out.println(sum);
+		out.println("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		System.out.println("doPost 시작");
+		doGet(request, response); //get을 호출함
+	}
+ 
+}
